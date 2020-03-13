a7b66006e8a00baea5c8bd570420e98cfb5c402a
fc940411 <1073440439@qq.com>
2020-03-13 16:40:16 +0800

    V0.0.1 Arrow_War

--- Arrow_War/apps/users/urls.py
+++ Arrow_War/apps/users/urls.py
@@ -1,8 +1,9 @@
-from apps.users.views import Index, Register
+from apps.users.views import Index, Register, Personal
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^index$', Index.as_view(),name='index'),
-    url(r'^register$', Register.as_view(),name='register'),
-    url(r'', Index.as_view()),
+    url(r'^index$', Index.as_view(), name='index'),
+    url(r'^register$', Register.as_view(), name='register'),
+	url(r'^personal$', Personal.as_view(), name='personal'),
+    url(r'^$', Index.as_view(), name='empty'),
 ]
